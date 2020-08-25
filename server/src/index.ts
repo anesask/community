0aeaeec29af196a2df55101c60f989ed4e9c3e93
Anes Mulalic <anes.mulalic@hotmail.com>
2020-08-25 18:37:36 +0200

    MikroORM Setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+const main = async () => {
+    // ORM Instance
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, { title: 'My First Post' });
+    // await orm.em.persistAndFlush(post);
+    // console.log(post);
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+};
+
+main().catch((err) => {
+    console.error(err);
+});
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { Post } from './entities/Post';
+import { __prod__ } from './constants';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+console.log("dirname:", __dirname);
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: 'community_v1',
+    type: 'postgresql',
+    user: 'postgres',
+    password: 'administrator',
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
