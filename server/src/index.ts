371c47af627eaab92a4e4cd25373ea6a32da1bb9
Anes Mulalic <anes.mulalic@hotmail.com>
2020-08-26 19:40:12 +0200

    MikroORM TypeGraphQL CRUD

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import "reflect-metadata";
 import 'colorts/lib/string';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,60 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Int, Arg, Mutation } from 'type-graphql';
 import { MyContext } from 'src/types';
 import { Post } from '../entities/Post';
 
 @Resolver()
 export class PostResolver {
+    // Get Posts
     @Query(() => [Post])
     posts(@Ctx() { em }: MyContext): Promise<Post[]> {
         return em.find(Post, {});
+    }
+
+    // Get Post
+    @Query(() => Post, { nullable: true })
+    post(
+        @Arg('id', () => Int) id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        return em.findOne(Post, { id });
+    }
+
+    // Create Post
+    @Mutation(() => Post)
+    async createPost(
+        @Arg('title') title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        const post = em.create(Post, { title });
+        await em.persistAndFlush(post);
+        return post;
+    }
+
+    // Update Post
+    @Mutation(() => Post, { nullable: true })
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title', () => String, { nullable: true }) title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, { id });
+        if (!post) {
+            return null;
+        }
+        if (typeof title !== 'undefined') {
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+        return post;
+    }
+
+    // Delete Post
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<boolean> {
+        await em.nativeDelete(Post, { id });
+        return true;
     }
 }
