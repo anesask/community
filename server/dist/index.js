ca1bd8fc5ae8b958694308b2cb0b01e4890fbc25
Anes Mulalic <anes.mulalic@hotmail.com>
2020-08-26 13:57:26 +0200

    Apollo Server Express Setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,14 +12,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+require("reflect-metadata");
+require("colorts/lib/string");
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const posts = yield orm.em.find(Post_1.Post, {});
-    console.log(posts);
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+    apolloServer.applyMiddleware({ app });
+    app.listen(process.env.PORT, () => {
+        console.log(`Express Server started on port ${process.env.PORT}`.inverse);
+    });
 });
 main().catch((err) => {
     console.error(err);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,35 @@
+require('dotenv').config();
+import "reflect-metadata";
+import 'colorts/lib/string';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+
 const main = async () => {
     // ORM Instance
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, { title: 'My First Post' });
-    // await orm.em.persistAndFlush(post);
-    // console.log(post);
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+
+    const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+    apolloServer.applyMiddleware({ app });
+    app.listen(process.env.PORT, () => {
+        console.log(
+            `Express Server started on port ${process.env.PORT}`.inverse
+        );
+    });
 };
 
 main().catch((err) => {
